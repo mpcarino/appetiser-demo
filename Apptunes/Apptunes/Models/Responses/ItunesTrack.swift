6d75ccb127303a640dc3f778236e7d4ca5878ce8
marwincarino <81006256+mpscarino101@users.noreply.github.com>
2021-08-23 02:39:22 -0700

    Create ItunesTrack.swift

---
+++ Apptunes/Apptunes/Models/Responses/ItunesTrack.swift
@@ -0,0 +1,61 @@
+//
+//  ItunesTrack.swift
+//  Apptunes
+//
+//  Created by Marwin Carino on 8/22/21.
+//
+
+import Foundation
+
+struct ItunesTrack: Codable {
+    let type: String
+    let artistName: String
+    let collectionName: String
+    let trackName: String
+    let trackViewUrl: String
+    let previewUrl: String
+    let trackPrice: Double
+    let currency: String
+    let country: String
+    let artworkSmallUrl: String
+    let artworkMediumUrl: String
+    let artworkLargeUrl: String
+    let primaryGenreName: String
+    let longDescription: String
+    
+    enum CodingKeys: String, CodingKey {
+        case type = "wrapperType"
+        case artistName
+        case collectionName
+        case trackName
+        case trackViewUrl
+        case previewUrl
+        case trackPrice
+        case currency
+        case country
+        case artworkSmallUrl = "artworkUrl30"
+        case artworkMediumUrl = "artworkUrl60"
+        case artworkLargeUrl = "artworkUrl100"
+        case primaryGenreName
+        case longDescription = "description"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decodeIfPresent(String.self, forKey: .type) ?? ""
+        artistName = try container.decodeIfPresent(String.self, forKey: .artistName) ?? ""
+        collectionName = try container.decodeIfPresent(String.self, forKey: .collectionName) ?? ""
+        trackName = try container.decodeIfPresent(String.self, forKey: .trackName) ?? ""
+        trackViewUrl = try container.decodeIfPresent(String.self, forKey: .trackViewUrl) ?? ""
+        previewUrl = try container.decodeIfPresent(String.self, forKey: .previewUrl) ?? ""
+        trackPrice = try container.decodeIfPresent(Double.self, forKey: .trackPrice) ?? .zero
+        currency = try container.decodeIfPresent(String.self, forKey: .currency) ?? ""
+        country = try container.decodeIfPresent(String.self, forKey: .country) ?? ""
+        artworkSmallUrl = try container.decodeIfPresent(String.self, forKey: .artworkSmallUrl) ?? ""
+        artworkMediumUrl = try container.decodeIfPresent(String.self, forKey: .artworkMediumUrl) ?? ""
+        artworkLargeUrl = try container.decodeIfPresent(String.self, forKey: .artworkLargeUrl) ?? ""
+        primaryGenreName = try container.decodeIfPresent(String.self, forKey: .primaryGenreName) ?? ""
+        longDescription = try container.decodeIfPresent(String.self, forKey: .longDescription) ?? ""
+    }
+}
