3b283ef093460d9ee72d8778efd1ec0d6340fcd5
marwincarino <81006256+mpscarino101@users.noreply.github.com>
2021-08-23 02:39:29 -0700

    Update ItunesService.swift

--- Apptunes/Apptunes/Network/APIServices/ItunesService.swift
+++ Apptunes/Apptunes/Network/APIServices/ItunesService.swift
@@ -8,14 +8,30 @@
 import Foundation
 import Moya
 import PromiseKit
-
-typealias EmptyPromise = Promise<Void>
+import SwiftyJSON
 
 struct ItunesService: Requestable {
-    func search() -> EmptyPromise {
+    func getTracks() -> Promise<[ItunesTrack]> {
         let provider = MoyaProvider<MoyaItunesService>(plugins: [curlPlugin])
         let target = MoyaItunesService.search
         
-        return emptyRequest(provider: provider, target: target)
+        return callAPIRequest(provider: provider, target: target, response: [ItunesTrack].self, at: "results")
+    }
+
+    func downloadTracks(completion: @escaping ([ItunesTrack]) -> Void) {
+        if let url = URL(string: L10n.APIs.Url.root + L10n.APIs.Path.search) {
+            DispatchQueue.global().async {
+                if let string = try? String(contentsOf: url) {
+                    let json = JSON(parseJSON: string)
+                    let results = json["results"]
+                    if let decodedResponse = try? JSONDecoder().decode([ItunesTrack].self, from: results.rawData()) {
+                        UserDataManager.shared.itunesTracks = decodedResponse
+                    }
+                }
+                
+                completion(UserDataManager.shared.itunesTracks)
+            }
+        }
     }
 }
+
