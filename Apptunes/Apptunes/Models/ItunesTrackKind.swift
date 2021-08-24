c3d53a922b0ff10e9f631e0f1c7a28b406f8f1d1
marwincarino <mps.carino101@gmail.com>
2021-08-23 22:46:05 -0700

    Add ItunesTrackKind

---
+++ Apptunes/Apptunes/Models/ItunesTrackKind.swift
@@ -0,0 +1,13 @@
+//
+//  ItunesTrackKind.swift
+//  Apptunes
+//
+//  Created by Marwin Carino on 8/23/21.
+//
+
+import Foundation
+
+enum ItunesTrackKind: String {
+    case song
+    case movie = "feature-movie"
+}
